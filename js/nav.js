bc8b5d8e18e3ecdcba38dd28b3d0361d07ed73ee
LougerB <glouien@gmail.com>
2021-06-17 18:14:29 +0800

    added nav slide, need more tweaks

--- js/nav.js
+++ js/nav.js
@@ -18,7 +18,8 @@ const saberOpt = document.getElementById("saber"),
   beastOpt = document.getElementById("beast"),
   allOpt = document.getElementById("all"),
   navServants = document.getElementsByName("servant"),
-  searchBar = document.getElementById("search-bar");
+  searchBar = document.getElementById("search-bar"),
+  navServantIcons = document.getElementById("nav-menu-1");
 
 allOpt.checked = true;
 
@@ -90,9 +91,9 @@ export const filterServants = function (sWrapper, sName) {
   // SEARCH BAR
 
   // ====================================================
+
   searchBar.addEventListener("keyup", function (e) {
     const userSearch = e.target.value.toLowerCase();
-
     sName.forEach((names) => {
       const servantName = names.innerHTML;
       const servantCardEl =
@@ -117,3 +118,62 @@ const toggleHide = function (elem, addClass, removeClass) {
   elem.classList.remove(removeClass);
   elem.classList.add(addClass);
 };
+
+// =====================================================
+// Nav mobile
+// =====================================================
+
+navServantIcons.addEventListener("touchstart", startTouch, false);
+navServantIcons.addEventListener("touchmove", moveTouch, false);
+
+// Swipe Up / Down / Left / Right
+var initialX = null;
+var initialY = null;
+
+function startTouch(e) {
+  initialX = e.touches[0].clientX;
+  initialY = e.touches[0].clientY;
+}
+
+function moveTouch(e) {
+  if (initialX === null) {
+    return;
+  }
+
+  if (initialY === null) {
+    return;
+  }
+
+  var currentX = e.touches[0].clientX;
+  var currentY = e.touches[0].clientY;
+
+  var diffX = initialX - currentX;
+  var diffY = initialY - currentY;
+
+  if (Math.abs(diffX) > Math.abs(diffY)) {
+    // sliding horizontally
+    if (diffX > 0) {
+      // swiped left
+      navServantIcons.style.left = "-200px";
+      console.log("swiped left");
+    } else {
+      // swiped right
+      navServantIcons.style.left = "0";
+      console.log("swiped right");
+    }
+  } else {
+    // sliding vertically
+    if (diffY > 0) {
+      // swiped up
+      console.log("swiped up");
+    } else {
+      // swiped down
+      console.log("swiped down");
+    }
+  }
+
+  initialX = null;
+  initialY = null;
+
+  e.preventDefault();
+}
